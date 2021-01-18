e5f738b9facbda49a14dfa7e550e35ba78d4125f
hiroshi iwase <hiroshi_iwase@bita.com>
2021-01-18 18:29:14 +0900

    form.html作ってみた

--- index.js
+++ index.js
@@ -4,26 +4,42 @@ const server = http.createServer((req, res) => {
   const now = new Date();
   console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
 
   switch (req.method) {
     case 'GET':
-      res.write('GET ' + req.url);
+      const fs = require('fs');
+      const rs = fs.createReadStream('./form.html');
+      rs.pipe(res);
       break;
     case 'POST':
-      res.write('POST ' + req.url);
+      //res.write('POST ' + req.url);
       let rawData = '';
       req.on('data', (chunk) => {
         rawData = rawData + chunk;
+        //console.info('chunk は ' + chunk);
       }).on('end', () => {
-        console.info('[' + now + '] Data posted: ' + rawData);
+        //console.info('[' + now + '] Data posted: ' + rawData);
+        //const decoded = decodeURIComponent(rawData);
+        //console.info('[' + now + '] 投稿: ' + decoded);
+        const qs = require('querystring');
+        const answer = qs.parse(rawData);
+
+        const body2 = 'テスト：' + answer['name'];
+//        console.info('[' + body2 + '] ');
+
+        const body = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました';
+        console.info('[' + now + '] ' + body);
+        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+          //decoded + 'が投稿されました</h1></body></html>');
+          body + '</h1>' + body2 + '</body></html>');
+        res.end();
       });
       break;
     default:
       break;
   }
-  res.end();
 }).on('error', (e) => {
   console.error('[' + new Date() + '] Server Error', e);
 }).on('clientError', (e) => {
